e9b8f3152c0c09738ed6a61cfeb729781b9ed15f
rohan <rohankharel86@gmail.com>
2020-01-28 10:15:52 +0545

    Initial commit

---
+++ app/src/test/java/com/rohankharel/testing/MyTest.java
@@ -0,0 +1,17 @@
+package com.rohankharel.testing;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class MyTest {
+
+    @Test
+    public void testAddition(){
+        Arithmetic arithmetic = new Arithmetic();
+        float expectedResult = 10;
+        float actualResult = arithmetic.add(4, 6);
+        assertEquals(expectedResult, actualResult);
+    }
+}
